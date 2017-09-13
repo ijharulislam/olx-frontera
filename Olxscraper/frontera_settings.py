b8416d22fa6b29d87b755ac109e8687ba1dbd4ab
ijharulislam <ijharalkawsary@gmail.com>
2017-09-13 09:50:18 +0600

    Frontera revisiting

--- Olxscraper/frontera_settings.py
+++ Olxscraper/frontera_settings.py
@@ -1,4 +1,6 @@
-BACKEND = 'frontera.contrib.backends.sqlalchemy.FIFO'
+BACKEND = 'frontera.contrib.backends.sqlalchemy.revisiting.Backend'
 SQLALCHEMYBACKEND_ENGINE = 'sqlite:///olx_frontier.db'
 MAX_NEXT_REQUESTS = 10
 DELAY_ON_EMPTY = 0.0
+SQLALCHEMYBACKEND_REVISIT_INTERVAL = 10
+
--- Olxscraper/spiders/olx_all.py
+++ Olxscraper/spiders/olx_all.py
@@ -454,6 +454,7 @@ class OlxspiderSpider(scrapy.Spider):
                 # return
 
     def getDataUrl(self, response):
+        print("====== get Detail ======")
         main_cate = response.meta['main_cate']
 
         products = response.xpath(
